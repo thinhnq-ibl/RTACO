1261f29996ed67d1b8b5e11173bd008947d98014
thinhnguyen <thinhnq@blockchainlabs.asia>
2025-03-28 11:38:31 +0700

    hashG1

--- py/test.py
+++ py/test.py
@@ -1,11 +1,16 @@
+from py_ecc.bls12_381 import * 
+from hashlib import sha256
+import random
+
+p = field_modulus
+
 def FindYforX_BLS12381(x):
-    p = 4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559787
     beta = (pow(x, 3, p) + 4) % p
     y = pow(beta, (p + 1) // 4, p)
     return (beta, y)
 
 def sqrt_bls12381(beta):
-    p = 0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaab
+    
     # Check if beta is a quadratic residue (Euler's criterion)
     legendre = pow(beta, (p - 1) // 2, p)
     if legendre != 1:
@@ -14,9 +19,26 @@ def sqrt_bls12381(beta):
     y = pow(beta, (p + 1) // 4, p)
     return y
 
+def genRandom():
+	o = int(p)
+	return random.randint(2, o)
+
+def hashG1(byte_string):
+    beta = 0
+    y = 0
+    x = int.from_bytes(byte_string, "big") % curve_order
+    while True :
+        (beta, y) = FindYforX_BLS12381(x)
+        if beta == pow(y, 2, field_modulus) :
+            return (FQ(x), FQ(y))
+        x = (x + 1) % field_modulus
+
+
 (beta, y) = FindYforX_BLS12381(4)
 y = sqrt_bls12381(68)
 
 print('x:', 4)
 print('beta:', beta)
 print('y:', y)
+
+hashG1(b'hello')
