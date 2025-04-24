53e5c8e171aa4c4bed150585e68be46a4ca6e6e2
thinhnguyen <thinhnq@blockchainlabs.asia>
2025-04-24 17:38:00 +0700

    verify zkpok

--- py/TTP.py
+++ py/TTP.py
@@ -5,3 +5,106 @@ import random
 def genRandom():
 	o = int(curve_order)
 	return random.randint(2, o)
+
+def FindYforX(x) :
+    beta = (pow(x, 3, field_modulus) + 3) % field_modulus
+    y = pow(beta, (field_modulus + 1) //4, field_modulus)
+    return (beta, y)
+
+def hashG1(byte_string):
+    beta = 0
+    y = 0
+    x = int.from_bytes(byte_string, "big") % curve_order
+    while True :
+        (beta, y) = FindYforX(x)
+        if beta == pow(y, 2, field_modulus) :
+            return (FQ(x), FQ(y))
+        x = (x + 1) % field_modulus
+
+def encode_attributes(attr, encode_str):
+	o = int(curve_order)
+	encoded_attr = []
+	assert len(attr) == len(encode_str), "mismatch in encoding lengths"
+	for i in range(len(attr)):
+		if encode_str[i] == 1:
+			Chash = sha256(attr[i].encode("utf8").strip()).digest()
+			encoded_attr.append(int.from_bytes(Chash, "big") % o)
+		else:
+			encoded_attr.append(attr[i])
+	return encoded_attr
+
+def GenCommitment(params, encoded_attr):
+	_, g, o, hs = params 
+	Aw = [multiply(hs[i], encoded_attr[i]) for i in range(len(hs))]
+	comm = multiply(g, encoded_attr[len(hs)])
+	for i in range(0, len(Aw)):
+		comm = add(comm, Aw[i])
+	return comm
+
+def ttp_setup(q, ttp):
+	assert q > 0
+	hs = [hashG1((ttp+("h%s")% i).encode("utf8")) for i in range(q)]
+	return ((FQ, FQ2, FQ12), G1, int(curve_order), hs)
+
+def toChallenge(element_list):
+	"""Packages a challenge in a bijective way"""
+	Cstring = SHA256(element_list[0])
+	for i in range(1, len(element_list)):
+		Cstring += SHA256(element_list[i])
+	Chash = sha256(Cstring).digest()
+	return (int.from_bytes(Chash, "big"))
+
+def SHA256(element):
+	return sha256((element[0].n).to_bytes(32, 'big') + (element[1].n).to_bytes(32, 'big')).digest()
+
+def GenZKPoK(params, all_enc_attr, comm):
+	_, g, o, hs = params
+    # Generate random witnesses for the proof
+	total_wm = [random.randint(2, o) for _ in range(len(all_enc_attr))]
+	
+    # Compute commitments (Aw) using the witnesses
+	Aw = []
+	
+	# Compute commitment: g^{wm[-1]} * product(hs[j]^{wm[j]})
+	commitment = multiply(g, total_wm[-1])
+	for j in range(len(total_wm) - 1):
+		commitment = add(commitment, multiply(hs[j], total_wm[j]))
+	Aw.append(commitment)
+    
+    # Include the main commitment in the proof
+	comm_list = [comm]
+    
+    # Generate challenge using all relevant elements
+	element_list = [g] + Aw + comm_list + hs
+	c = toChallenge(element_list) % o
+    
+    # Compute responses
+	total_rm = []
+	for wm, attr in zip(total_wm, all_enc_attr):
+		total_rm.append((wm - c * attr) % o)
+    # print(total_wm)
+	return (c, total_rm)
+
+def VerifyZKPoK(params, encoded_attr, comm, ZKPoK):
+	c, total_rm = ZKPoK
+
+    # Check that all first responses are equal (if multiple attribute sets)
+	_, g, o, hs = params
+    
+    # Reconstruct the blinded commitment (tmp_comm)
+	tmp_comm = multiply(hs[1], encoded_attr[0])
+	for i in range(2, len(hs)):
+		tmp_comm = add(tmp_comm, multiply(hs[i], encoded_attr[i-1]))
+	tmp_comm = add(comm, neg(tmp_comm))  # comm - (sum hs[j] * attr[j])
+    
+    # Reconstruct Aw (commitments using responses)
+	Aw = []
+	commitment = multiply(g, total_rm[-1])
+	for j in range(len(total_rm) - 1):
+		commitment = add(commitment, multiply(hs[j], total_rm[j]))
+	commitment = add(commitment, multiply(comm, c))
+	Aw.append(commitment)
+    
+    # Generate challenge and verify
+	element_list = [g] + Aw + [comm] + hs
+	return (c == toChallenge(element_list) % o)
--- py/User.py
+++ py/User.py
@@ -1,3 +1,81 @@
 from TTP import *
+import datetime
+
 msk = genRandom()
 user_addr = "0x1A1684c3027eA12046155013BfC5518C65dD5943"
+
+vcert_title = "Identity Certificate"
+
+# RequestVcert(title, requiredVcerts = []):
+vcert = {"title":vcert_title, "attributes" : None, "commit": None, "signature": None}
+attributes = {}
+key1 = "msk"
+value1 = msk
+attributes.setdefault(key1, value1)
+key2 = "r"
+value2 = genRandom()
+attributes.setdefault(key2, value2)
+key3 = "name"
+value3 = "Justin"
+attributes.setdefault(key3, value3)
+key4 = "dob"
+value4 = "1998-05-12"
+attributes.setdefault(key4, value4)
+
+			
+attribute = []
+encode_str = []
+
+# make order for schema order
+schemaOrder = ["msk", "name", "dob", "r"]
+# encode type 1: string, 2: int, 3: datetime
+# prv key
+attribute.append(attributes[key1])
+encode_str.append(2) # int
+# name
+attribute.append(attributes[key3])
+encode_str.append(1) # string
+# DOB
+_date = datetime.datetime.strptime(value4,"%Y-%m-%d").date()
+value = int(_date.strftime('%Y%m%d'))
+attribute.append(value)
+encode_str.append(3) # datetime
+# r
+attribute.append(attributes[key2])
+encode_str.append(2) # int
+
+	
+encoded_attribute = encode_attributes(attribute, encode_str)
+q = len(schemaOrder)
+
+params = ttp_setup(q-1, vcert_title) # exclude r.
+
+commit = GenCommitment(params, encoded_attribute)
+
+zkpok = GenZKPoK(params, encoded_attribute, commit)
+
+# send for CA do verify
+
+result  = VerifyZKPoK(params, encoded_attribute, commit, zkpok)
+print("ZKPoK verification result: ", result)
+
+	# 	requestJSON = jsonpickle.encode((prevCombination, prevVcerts, attributes, commit, zkpok))
+	# 	s.send(requestJSON.encode())
+	# 	issueVcertJSON = s.recv(8192).decode()
+
+	# 	issueVcert = jsonpickle.decode(issueVcertJSON)
+	# 	_commit, signature = issueVcert
+
+	# 	if commit != _commit:
+	# 		print("Request is corrupted.")
+	# 	elif VerifyVcerts(params, pk, signature, SHA256(commit)) == True:
+	# 		vcert["attributes"] = attributes
+	# 		vcert["commit"] = commit
+	# 		vcert["signature"] = signature
+	# 	else:
+	# 		print("Request is corrupted.")
+	# except Exception as e:
+	# 	print(e)
+	# finally:
+	# 	s.close()
+	# return vcert
