56a0c4c2cc77c1115e5c1eacd9ecf506f6015360
thinhnguyen <thinhnq@blockchainlabs.asia>
2025-04-25 10:19:36 +0700

    send request certificate

--- py/TTP.py
+++ py/TTP.py
@@ -178,3 +178,213 @@ def ttpKeyGen(params):
 
 def VerifyVcerts(params, pk, sign, digest):
 	return do_ecdsa_verify(pk, sign, digest)
+
+def ttp_keygen(params, t, n):
+    (G, o, g1, hs, g2, e) = params
+    q = len(hs)
+    assert n >= t and t > 0 and q > 0
+    # generate polynomials
+    v = [random.randint(2, o) for _ in range(0,t)]
+    w = [[random.randint(2, o) for _ in range(0,t)] for _ in range(q)]
+    # generate shares
+    x = [poly_eval(v,i) % o for i in range(1,n+1)]
+    y = [[poly_eval(wj,i) % o for wj in w] for i in range(1,n+1)]
+    # set keys
+    sk = list(zip(x, y))
+    vk = [(g2, multiply(g2, x[i]), [multiply(g1, y[i][j]) for j in range(len(y[i]))], [multiply(g2, y[i][j]) for j in range(len(y[i]))]) for i in range(len(sk))]
+    return (sk, vk)
+
+def agg_key(params, vks):
+    (G, o, g1, hs, g2, e) = params
+    # filter missing keys (in the threshold setting)
+    filter = [vks[i] for i in range(len(vks)) if vks[i] is not None]
+    indexes = [i+1 for i in range(len(vks)) if vks[i] is not None]
+    # evaluate all lagrange basis polynomials
+    l = lagrange_basis(indexes,o)
+    # aggregate keys
+    (_, alpha, g1_beta, beta) = zip(*filter)
+    q = len(beta[0])
+    aggr_alpha = ec_sum([multiply(alpha[i], l[i]) for i in range(len(filter))])
+    aggr_g1_beta = [ec_sum([multiply(g1_beta[i][j], l[i]) for i in range(len(filter))]) for j in range(q)]
+    aggr_beta = [ec_sum([multiply(beta[i][j], l[i]) for i in range(len(filter))]) for j in range(q)]
+    aggr_vk = (g2, aggr_alpha, aggr_g1_beta, aggr_beta)
+    return aggr_vk
+
+def lagrange_basis(indexes, o, x=0):
+    """ generates all lagrange basis polynomials """
+    l = []
+    for i in indexes:
+        numerator, denominator = 1, 1
+        for j in indexes:
+            if j != i:
+                numerator = (numerator * (x - j)) % o
+                denominator = (denominator * (i - j)) % o
+        l.append((numerator * modInverse(denominator, o)) % o)
+    return l
+
+def setup(q=1, AC = "h"):
+    assert q > 0
+    hs = [hashG1((AC+"%s"%i).encode("utf8")) for i in range(q)]
+    return ((FQ, FQ2, FQ12), curve_order, G1, hs, G2, pairing)
+
+def poly_eval(coeff, x):
+    """ evaluate a polynomial defined by the list of coefficient coeff at point x """
+    return sum([coeff[i] * ((x) ** i) for i in range(len(coeff))])
+
+def ec_sum(list):
+    """ sum EC points list """
+    ret = None
+    if len(list) != 0:
+        ret = list[0]
+    for i in range(1,len(list)):
+        ret = add(ret, list[i])
+    return ret
+
+def opener_keygen(params):
+    (_, o, _, _, g2, _) = params
+    z = random.randint(2, o)
+    f = multiply(g2, z)
+    return (f, z)
+
+def encodeG2(g2):
+	return (g2[0].coeffs[0].n, g2[0].coeffs[1].n, g2[1].coeffs[0].n, g2[1].coeffs[1].n)
+
+def PrepareCredRequest(params, aggr_vk, to, no, opk, prevParams, all_attr, include_indexes, public_m=[]):
+    private_m = []
+    # for i in range(len(all_attr)):
+    #     for j in range(len(all_attr[i])):
+    #         if include_indexes[i][j] == 1:
+    #             private_m.append(int(all_attr[i][j]))
+    private_m.append(all_attr[0][0])
+    private_m.append(all_attr[0][3])
+    public_m.append(all_attr[0][1])
+    public_m.append(all_attr[0][2])
+    assert len(private_m) > 0
+    (G, o, g1, hs, g2, e) = params
+    attributes = private_m + public_m
+    assert len(attributes) <= len(hs)
+    # build commitment
+    rand = random.randint(2, o)#generates random number 
+    cm = add(multiply(g1, rand), ec_sum([multiply(hs[i], attributes[i]) for i in range(len(attributes))]))
+    # build El Gamal encryption
+    h = hashG1(to_binary256(cm))
+    os = [random.randint(2, o) for _ in range(len(private_m))]#os is a "private_m" length random number array
+    commitments = [add(multiply(g1, os[i]), multiply(h, private_m[i])) for i in range(len(private_m))]
+    pi_s = make_pi_s(params, commitments, cm, os, rand, public_m, private_m, all_attr, prevParams, include_indexes)
+    # build proofs
+    # pi_s = make_pi_s(params, gamma, c, cm, k, r, public_m, private_m)
+    # Lambda = (cm, c, pi_s)
+    # opening information
+    # generate polynomials to hide private attributes (m polynomials of degree 'to')
+    P = [[random.randint(2, o) for _ in range(0, to)] for _ in range(len(private_m))]
+    for i in range(len(private_m)):
+        P[i][0] = private_m[i]
+    #generate shares s[i] contains shares to ne shared with opener 'i'
+    s = [[poly_eval(Pj,i) % o for Pj in P] for i in range(1,no+1)]
+    hidden_P = [[multiply(h, P[i][j]) for j in range(1, to)] for i in range(len(private_m))]
+
+    _, _, _, beta = aggr_vk
+    r = [random.randint(2, o) for _ in range(no)]
+    C = [(multiply(g2, r[i]), (add(multiply(opk[i], r[i]), ec_sum([multiply(beta[j], s[i][j]) for j in range(len(private_m))])))) for i in range(no)]
+    
+    Aw, Bw, pi_o = make_pi_o(params, cm, C, r, s, aggr_vk, opk)
+    
+    h_r = [multiply(h, ri) for ri in r]
+    b_o = [multiply(beta[i], os[i]) for i in range(len(os))] 
+
+    Lambda = (cm, commitments, pi_s, hidden_P, C, pi_o, Aw, Bw, h_r, b_o)
+    return Lambda, os
+
+def to_binary256(point) :
+    if isinstance(point, str):
+        return sha256(point.encode("utf8").strip()).digest()
+    if isinstance(point, int):
+        return point.to_bytes(32, 'big')
+    if isinstance(point[0], FQ):
+        point1 = point[0].n.to_bytes(32, 'big')
+        point2 = point[1].n.to_bytes(32, 'big')
+        return sha256(point1+point2).digest()
+    if isinstance(point[0], FQ2):
+        point1 = point[0].coeffs[0].n.to_bytes(32, 'big') + point[0].coeffs[1].n.to_bytes(32, 'big')
+        point2 = point[1].coeffs[0].n.to_bytes(32, 'big') + point[1].coeffs[1].n.to_bytes(32, 'big')
+        return sha256(point1+point2).digest()
+
+def make_pi_s(params, commitments, cm, os, r, public_m, private_m, all_attr, prevParams, include_indexes):
+    """ prove correctness of ciphertext and cm """
+    (G, o, g1, hs, g2, e) = params
+    attributes = private_m + public_m
+
+    assert len(commitments) == len(os) and len(commitments) == len(private_m)
+    assert len(attributes) <= len(hs)
+    # create the witnesses
+    wr =random.randint(2, o)
+    wos = [random.randint(2, o) for _ in os]
+    total_wm = [[random.randint(2, o) for _ in range(len(all_attr[i]))] for i in range(len(all_attr))]
+    wm = []
+    for i in range(len(all_attr)):
+        for j in range(len(all_attr[i])):
+            if include_indexes[i][j] == 1:
+                wm.append(int(total_wm[i][j]))
+    pub_wm = []
+    for _ in public_m:
+        pub_wm.append(random.randint(2,o))
+    wm = wm + pub_wm
+    total_wm.append(pub_wm)
+    for i in range(1, len(total_wm)-1):
+        total_wm[i][0] = total_wm[0][0]
+    # compute h
+    h = hashG1(to_binary256(cm))
+    # compute the witnesses commitments
+    Aw = [add(multiply(g1, wos[i]), multiply(h, wm[i])) for i in range(len(private_m))]
+    Bw = add(multiply(g1, wr), ec_sum([multiply(hs[i], wm[i]) for i in range(len(attributes))]))
+    Cw = []
+    for i in range(len(total_wm) - 1):
+        (_, ttp_g, _, ttp_hs) = prevParams[i]
+        tmp = multiply(ttp_g, total_wm[i][-1])
+        for j in range(len(total_wm[i]) - 1):
+            tmp = add(tmp, multiply(ttp_hs[j], total_wm[i][j]))
+        Cw.append(tmp)
+    # create the challenge
+    c = to_challenge([g1, g2, cm, h, Bw]+hs+Aw+Cw)
+    # create responses
+    rr = (wr - c * r) % o
+    ros = [(wos[i] - c*os[i]) % o for i in range(len(wos))]
+    total_rm = [[(total_wm[i][j] - c*all_attr[i][j]) % o for j in range(len(total_wm[i]))] for i in range(len(total_wm) - 1)]
+    total_rm.append([(total_wm[-1][i] - c*public_m[i]) % o for i in range(len(total_wm[-1]))])
+    # rm = [(wm[i] - c*attributes[i]) % o for i in range(len(wm))]
+    return (c, rr, ros, total_rm)
+
+
+def to_challenge(elements):
+    _list = [to_binary256(x) for x in elements]
+    Cstring = _list[0]
+    for i in range(1, len(_list)):
+        Cstring += _list[i]
+    Chash =  sha256(Cstring).digest()
+    return int.from_bytes(Chash, "big", signed=False)
+
+def make_pi_o(params, cm, C, r, s, aggr_vk, opk):
+    (G, o, g1, hs, g2, e) = params
+
+    # assert len(ciphertext) == len(k) and len(ciphertext) == len(private_m)
+    # assert len(ciphershares) == len(opk)
+    # assert len(attributes) <= len(hs)
+    # create the witnesses
+
+    wr = [random.randint(2, o) for _ in r]
+    ws = [[random.randint(2, o) for _ in s[i]] for i in range(len(s))]
+    # compute h
+    h = hashG1(to_binary256(cm))
+    _, _, _, beta = aggr_vk
+    # compute the witnesses commitments
+    Aw = [multiply(g2, wri) for wri in wr]
+    Bw = [add(multiply(opk[i], wr[i]), ec_sum([multiply(beta[j], ws[i][j]) for j in range(len(ws[i]))])) for i in range(len(ws))]
+
+    # create the challenge
+    c = []
+    for i in range(len(wr)):
+        c.append(to_challenge([g1, g2, h, Aw[i], Bw[i]]+ hs))
+
+    rr = [(wr[i] - c[i]*r[i]) % o for i in range(len(wr))] 
+    rs = [[(ws[i][j] - c[i]*s[i][j])% o for j in range(len(s[i]))] for i in range(len(s))]
+    return (Aw, Bw, (c, rr, rs))
--- py/User.py
+++ py/User.py
@@ -48,25 +48,81 @@ encode_str.append(2) # int
 encoded_attribute = encode_attributes(attribute, encode_str)
 q = len(schemaOrder)
 
-params = ttp_setup(q-1, vcert_title) # exclude r.
+ca_params = ttp_setup(q-1, vcert_title) # exclude r.
 
-commit = GenCommitment(params, encoded_attribute)
+commit = GenCommitment(ca_params, encoded_attribute)
 
-zkpok = GenZKPoK(params, encoded_attribute, commit)
+zkpok = GenZKPoK(ca_params, encoded_attribute, commit)
 
 # send for CA do verify
 
-result  = VerifyZKPoK(params, encoded_attribute, commit, zkpok)
+result  = VerifyZKPoK(ca_params, encoded_attribute, commit, zkpok)
 print("ZKPoK verification result: ", result)
 
-pubCP, mskCP = ttpKeyGen(params)
-signature = SignCommitment(params, mskCP, commit)
+pubCP, mskCP = ttpKeyGen(ca_params)
+signature = SignCommitment(ca_params, mskCP, commit)
 issueVcert = (commit, signature)
 print("Signature: ", signature)
 
-if(VerifyVcerts(params, pubCP, signature, SHA256(commit)) == True):
+if(VerifyVcerts(ca_params, pubCP, signature, SHA256(commit)) == True):
     vcert["attributes"] = attributes
     vcert["commit"] = commit
     vcert["signature"] = signature
         
 print(vcert)
+
+ac_title = "Loan Credential"
+
+params = setup(q, ac_title)
+(sk, vk) = ttp_keygen(params, 1, 1)
+aggregate_vk = agg_key(params, vk)
+to = 1
+no = 1
+(opk, osk) = opener_keygen(params)
+opks = [opk]
+prevVcerts = []	
+prevParams = []
+all_encoded_attr = []
+prevParams.append(ca_params)
+prevVcerts.append((vcert["commit"], vcert["signature"]))
+all_encoded_attr.append(encoded_attribute)
+
+combination = ["Identity Certificate"]
+include_indexes = [[1,0,0,1]]
+
+Lambda, os = PrepareCredRequest(params, aggregate_vk, to, no, opks, prevParams, all_encoded_attr, include_indexes, public_m=[])
+	
+print("Lambda: ", Lambda)
+print("os: ", os)
+
+(cm, commitments, pi_s, hp, C, pi_o, Dw, Ew, hr, bo) = Lambda
+#anything with "send" appended is making that particular variable as SC compatible.
+send_cm = (cm[0].n, cm[1].n)
+send_commitments = [(commitments[i][0].n, commitments[i][1].n) for i in range(len(commitments))]
+send_ciphershares= [([([C[i][j][0].coeffs[1].n,C[i][j][0].coeffs[0].n],[C[i][j][1].coeffs[1].n, C[i][j][1].coeffs[0].n]) for j in range(2)],) for i in range(len(C))]
+send_compressed_cipher = (send_commitments, send_ciphershares)
+private_m = []
+# schema = downloadSchema(title)
+# schemaOrder = downloadSchemaOrder(title)
+# for key in schemaOrder:
+#     if schema[key]['visibility'] == 'private':
+#         private_m.append(credential["attributes"][key])
+private_m.append(vcert["attributes"][key1])
+private_m.append(vcert["attributes"][key2])
+send_hp =  [[(hp[i][j-1][0].n, hp[i][j-1][1].n) for j in range(1, to)] for i in range(len(private_m))]
+send_hr = [(hr[i][0].n, hr[i][1].n) for i in range(len(hr))]
+send_bo = [([bo[i][0].coeffs[1].n,bo[i][0].coeffs[0].n],[bo[i][1].coeffs[1].n,bo[i][1].coeffs[0].n]) for i in range(len(bo))]
+send_Dw = [([Dw[i][0].coeffs[1].n,Dw[i][0].coeffs[0].n],[Dw[i][1].coeffs[1].n,Dw[i][1].coeffs[0].n]) for i in range(len(Dw))]
+send_Ew = [([Ew[i][0].coeffs[1].n,Ew[i][0].coeffs[0].n],[Ew[i][1].coeffs[1].n,Ew[i][1].coeffs[0].n]) for i in range(len(Ew))]
+send_compressed_G2Points = (send_Dw, send_Ew)
+send_vcerts = [((prevVcerts[i][0][0].n, prevVcerts[i][0][1].n), prevVcerts[i][1]) for i in range(len(prevVcerts))]
+
+pi_s = list(pi_s)
+pi_s.append(combination)
+pi_s = tuple(pi_s)
+
+print("sending for verification")
+public_m = []
+public_m.append(vcert["attributes"][key3])
+public_m.append(vcert["attributes"][key4])
+str_public_m = [str(public_m[i]) for i in range(len(public_m))]
