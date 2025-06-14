3b69ad2f5aa65c0df67b8fd87fb6e3fa7b53fd10
thinhnguyen <thinhnq@blockchainlabs.asia>
2025-06-14 16:51:17 +0700

    refactor code

--- py/simple-vcert-lock.py
+++ py/simple-vcert-lock.py
@@ -4,7 +4,6 @@ from pycardano import (
     BlockFrostChainContext,
     Network,
     PaymentSigningKey,
-    PaymentVerificationKey,
     PlutusData,
     PlutusV3Script,
     ScriptHash,
@@ -12,115 +11,18 @@ from pycardano import (
     TransactionOutput,
 )
 from pycardano.hash import (
-    VerificationKeyHash,
     TransactionId,
     ScriptHash,
 )
 import json
-import os
 
 from TTP_bls12 import *
-import datetime
-import os
 
-from blockfrost import ApiUrls, BlockFrostApi
-from dotenv import load_dotenv
-from pycardano import Address, Network, crypto, ExtendedSigningKey, PlutusV3Script, TransactionBuilder, TransactionOutput, plutus_script_hash, BlockFrostChainContext, Redeemer, PlutusData
-import cbor2
-from retry import retry
+from pycardano import Address, Network, PlutusV3Script, TransactionBuilder, TransactionOutput, BlockFrostChainContext, PlutusData
 import json
-
-
-from py_ecc.bls.hash import (
-    i2osp,
-    os2ip
-)
-from py_ecc.bls.point_compression import (
-    G1Uncompressed
-)
-from py_ecc.fields import (
-    optimized_bls12_381_FQ as FQO,
-)
 
 from typing import List, Dict
 from dataclasses import dataclass
-
-##################################
-## create vcert
-##################################
-
-msk = genRandom()
-user_addr = "0x1A1684c3027eA12046155013BfC5518C65dD5943"
-
-# Identity Certificate
-vcert_title = "Identity Certificate"
-
-vcert = {"title":vcert_title, "attributes" : None, "commit": None, "signature": None}
-attributes = {}
-key1 = "msk"
-value1 = msk
-attributes.setdefault(key1, value1)
-key2 = "r"
-value2 = genRandom()
-attributes.setdefault(key2, value2)
-key3 = "name"
-value3 = "Justin"
-attributes.setdefault(key3, value3)
-key4 = "dob"
-value4 = "1998-05-12"
-attributes.setdefault(key4, value4)
-			
-attribute = []
-encode_str = []
-
-# make order for schema order
-schemaOrder = ["msk", "name", "dob", "r"]
-# encode type 1: string, 2: int, 3: datetime
-# prv key
-attribute.append(attributes[key1])
-encode_str.append(2) # int
-# name
-attribute.append(attributes[key3])
-encode_str.append(1) # string
-# DOB
-_date = datetime.datetime.strptime(value4,"%Y-%m-%d").date()
-value = int(_date.strftime('%Y%m%d'))
-attribute.append(value)
-encode_str.append(3) # datetime
-# r
-attribute.append(attributes[key2])
-encode_str.append(2) # int
-
-	
-encoded_attribute = encode_attributes(attribute, encode_str)
-q = len(schemaOrder)
-
-ca_params = ttp_setup(q-1, vcert_title) # exclude r.
-
-# commit is G1 point
-commit = GenCommitment(ca_params, encoded_attribute)
-
-zkpok = GenZKPoK(ca_params, [], [], [encoded_attribute], commit)
-
-# send for CA do verify
-encoded_attribute_verify = [encoded_attribute[1], encoded_attribute[2]]
-result  = VerifyZKPoK(ca_params, [], [], encoded_attribute_verify, commit, zkpok)
-#print("ZKPoK verification result: ", result)
-
-
-pubCP, mskCP = ttpKeyGen(ca_params)
-signature = SignCommitment(ca_params, mskCP, commit)
-issueVcert = (commit, signature)
-# print("pubCP: ", pubCP)
-pubkeyUncompress: G1Uncompressed = (FQO(pubCP[0].n),
-                              FQO(pubCP[1].n), 
-                              FQO(1))
-
-
-if(VerifyVcerts(ca_params, pubCP, signature, SHA256(commit)) == True):
-    vcert["attributes"] = attributes
-    vcert["commit"] = commit
-    vcert["signature"] = signature
 
 @dataclass
 class G2Point(PlutusData):
