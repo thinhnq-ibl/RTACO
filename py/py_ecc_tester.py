9690790e5a8fa613bc22ceba27bc8ecb386b1989
thinhnguyen <thinhnq@blockchainlabs.asia>
2025-07-31 14:39:49 +0700

    create iith

--- py/py_ecc_tester.py
+++ py/py_ecc_tester.py
@@ -101,28 +101,28 @@ def ec_sum(list):
         ret = add(ret, list[i])
     return ret
 
-def modInverse(a, m):
-    m0 = m
-    y = 0
-    x = 1 
-    if (m == 1):
-        return 0
-    while (a > 1):
-        # q is quotient
-        q = a // m
-        t = m
-        # m is remainder now, process
-        # same as Euclid's algo
-        m = a % m
-        a = t
-        t = y
-        # Update x and y
-        y = x - q * y
-        x = t
-    # Make x positive
-    if (x < 0):
-        x = x + m0
-    return x
+# def modInverse(a, m):
+#     m0 = m
+#     y = 0
+#     x = 1 
+#     if (m == 1):
+#         return 0
+#     while (a > 1):
+#         # q is quotient
+#         q = a // m
+#         t = m
+#         # m is remainder now, process
+#         # same as Euclid's algo
+#         m = a % m
+#         a = t
+#         t = y
+#         # Update x and y
+#         y = x - q * y
+#         x = t
+#     # Make x positive
+#     if (x < 0):
+#         x = x + m0
+#     return x
 
 def lagrange_basis(indexes, o, x=0):
     """ generates all lagrange basis polynomials """
