3a32e6ea6c0e98d1ff96bee6459dbc5a04c4c0a2
thinhnguyen <thinhnq@blockchainlabs.asia>
2025-09-15 12:10:46 +0700

    income schema

--- js/attribute-certifier.js
+++ js/attribute-certifier.js
@@ -23,4 +23,24 @@ const createIdentitySchema = async (db) => {
   console.log("SCHEMA created:", newSchema);
 };
 
+const createIncomeSchema = async (db) => {
+  if (db.schemas.find((s) => s.name === "INCOME")) {
+    console.log("Schema already exists:", "INCOME");
+    return;
+  }
+  let data = await runPythonScript("../py/attribute-certifier.py", [
+    "genIncomeSchema",
+  ]);
+  console.log("Generated schema:", data.schema);
+  const newSchema = {
+    name: "INCOME",
+    schema: data.schema,
+    schemaOrder: data.schemaOrder,
+  };
+  db.schemas.push(newSchema);
+  fs.writeFileSync("db.json", JSON.stringify(db, null, 2));
+  console.log("SCHEMA created:", newSchema);
+};
+
 // createIdentitySchema(db);
+// createIncomeSchema(db);
--- py/attribute-certifier.py
+++ py/attribute-certifier.py
@@ -6,28 +6,45 @@ import sys
 import json
 
 out = {}
+encoding_type_map = {"1": type("string"), "2": type(1), "3": type(datetime.datetime.now())}
 
 def genIdentitySchema():
     schema = {}
-    encoding = {}
     schemaOrder = []
    
-    encoding_type_map = {"1": type("string"), "2": type(1), "3": type(datetime.datetime.now())}
-
     key = "msk"
     schemaOrder.append(key)
     schema.setdefault(key, {"type" : "2", "visibility": "private"})
-    encoding.setdefault(key, 2)
 
     key = "name"
     schemaOrder.append(key)
     schema.setdefault(key, {"type" : "1", "visibility": "private"})
-    encoding.setdefault(key, 1)
 
     key = "dob"
     schemaOrder.append(key)
     schema.setdefault(key, {"type" : "3", "visibility": "private"})
-    encoding.setdefault(key, 3)
+
+    key = "r"
+    schemaOrder.append(key)
+    schema.setdefault(key, {"type" : "2", "visibility": "private"})
+
+    return {"schema": schema, "schemaOrder": schemaOrder}
+
+def genIncomeSchema():
+    schema = {}
+    schemaOrder = []
+
+    key = "msk"
+    schemaOrder.append(key)
+    schema.setdefault(key, {"type" : "2", "visibility": "private"})
+
+    key = "organization"
+    schemaOrder.append(key)
+    schema.setdefault(key, {"type" : "1", "visibility": "private"})
+
+    key = "salary"
+    schemaOrder.append(key)
+    schema.setdefault(key, {"type" : "2", "visibility": "private"})
 
     key = "r"
     schemaOrder.append(key)
@@ -43,6 +60,11 @@ if __name__ == "__main__":
         name = sys.argv[1]
         if name == "genIdentitySchema":
             result = genIdentitySchema()
+            out["schema"] = result["schema"]
+            out["schemaOrder"] = result["schemaOrder"]
+            print(json.dumps(out))
+        elif name == "genIncomeSchema":
+            result = genIncomeSchema()
             out["schema"] = result["schema"]
             out["schemaOrder"] = result["schemaOrder"]
             print(json.dumps(out))
